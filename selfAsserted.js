159f586bb6c942a4550f5a46c6d3a0fa41eff4a8
To Nguyen <84770313+to-innomizetech@users.noreply.github.com>
2025-03-10 19:04:50 +0700

    Add Terms & Privacy

--- selfAsserted.js
+++ selfAsserted.js
@@ -1,15 +1,53 @@
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('===========================================');
-  console.log('Start custom');
-  console.log('===========================================');
+// document.addEventListener('DOMContentLoaded', function () {
+//   console.log('===========================================');
+//   console.log('Start custom');
+//   console.log('===========================================');
 
-  var observer = new MutationObserver(function (mutations, obs) {
-    var continueButton = document.querySelector("button[type='submit']");
+//   var observer = new MutationObserver(function (mutations, obs) {
+//     var continueButton = document.querySelector("button[type='submit']");
+//     console.log('===========================================');
+//     console.log('continueButton: ', continueButton);
+//     console.log('===========================================');
+//     if (continueButton) {
+//       obs.disconnect();
+
+//       var checkboxContainer = document.createElement('div');
+//       checkboxContainer.innerHTML = `
+//               <div style="margin-top: 15px;">
+//                   <input type="checkbox" id="termsCheckbox">
+//                   <label for="termsCheckbox">
+//                       I agree to the <a href="https://policies.google.com/" target="_blank">Terms & Privacy</a>
+//                   </label>
+//               </div>
+//           `;
+
+//       continueButton.parentNode.insertBefore(checkboxContainer, continueButton);
+
+//       continueButton.disabled = true;
+//       document
+//         .getElementById('termsCheckbox')
+//         .addEventListener('change', function () {
+//           continueButton.disabled = !this.checked;
+//         });
+//     }
+//   });
+
+//   observer.observe(document.body, { childList: true, subtree: true });
+// });
+
+
+function setupPwdTogglers(){
+    console.log('===========================================');
+    console.log('Start custom');
+    console.log('===========================================');
+    while (!continueButton) {
+      console.log('querySelector button submit');
+      var continueButton = document.querySelector("button[type='submit']");
+    }
     console.log('===========================================');
     console.log('continueButton: ', continueButton);
     console.log('===========================================');
     if (continueButton) {
-      obs.disconnect();
 
       var checkboxContainer = document.createElement('div');
       checkboxContainer.innerHTML = `
@@ -23,14 +61,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
       continueButton.parentNode.insertBefore(checkboxContainer, continueButton);
 
-      continueButton.disabled = true;
+      continueButton.setAttribute("aria-disabled", "true");
+      continueButton.style.pointerEvents = "none"
+      continueButton.style.opacity = "0.3"; 
+
       document
         .getElementById('termsCheckbox')
         .addEventListener('change', function () {
-          continueButton.disabled = !this.checked;
+          if (this.checked) {
+            continueButton.setAttribute("aria-disabled", "false");
+            continueButton.style.pointerEvents = "auto"
+            continueButton.style.opacity = "1";
+          } else {
+            continueButton.setAttribute("aria-disabled", "true");
+            continueButton.style.pointerEvents = "none"
+            continueButton.style.opacity = "0.3"; 
+          }  
         });
     }
-  });
+}
 
-  observer.observe(document.body, { childList: true, subtree: true });
-});
+setupPwdTogglers();
