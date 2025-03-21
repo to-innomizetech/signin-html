04954ffdd46e9bb32ce53d5a3289b2756849d4f3
To Nguyen <conhxyz@jmfamily.com>
2025-03-21 17:36:57 +0700

    Update lint Privacy policy

--- selfAsserted.js
+++ selfAsserted.js
@@ -35,50 +35,48 @@
 //   observer.observe(document.body, { childList: true, subtree: true });
 // });
 
-
-function setupPwdTogglers(){
-    console.log('===========================================');
-    console.log('Start custom');
-    console.log('===========================================');
-    while (!continueButton) {
-      console.log('querySelector button submit');
-      var continueButton = document.querySelector("button[type='submit']");
-    }
-    console.log('===========================================');
-    console.log('continueButton: ', continueButton);
-    console.log('===========================================');
-    if (continueButton) {
-
-      var checkboxContainer = document.createElement('div');
-      checkboxContainer.innerHTML = `
+function setupPwdTogglers() {
+  console.log('===========================================');
+  console.log('Start custom');
+  console.log('===========================================');
+  while (!continueButton) {
+    console.log('querySelector button submit');
+    var continueButton = document.querySelector("button[type='submit']");
+  }
+  console.log('===========================================');
+  console.log('continueButton: ', continueButton);
+  console.log('===========================================');
+  if (continueButton) {
+    var checkboxContainer = document.createElement('div');
+    checkboxContainer.innerHTML = `
               <div style="margin-top: 15px;">
                   <input type="checkbox" id="termsCheckbox">
                   <label for="termsCheckbox">
-                      I agree to the <a href="https://policies.google.com/" target="_blank">Terms & Privacy</a>
+                      I agree to the <a href="https://baqa-frontend-dev.azurewebsites.net/privacy-policy" target="_blank">Terms & Privacy</a>
                   </label>
               </div>
           `;
 
-      continueButton.parentNode.insertBefore(checkboxContainer, continueButton);
+    continueButton.parentNode.insertBefore(checkboxContainer, continueButton);
 
-      continueButton.setAttribute("aria-disabled", "true");
-      continueButton.style.pointerEvents = "none"
-      continueButton.style.opacity = "0.3"; 
+    continueButton.setAttribute('aria-disabled', 'true');
+    continueButton.style.pointerEvents = 'none';
+    continueButton.style.opacity = '0.3';
 
-      document
-        .getElementById('termsCheckbox')
-        .addEventListener('change', function () {
-          if (this.checked) {
-            continueButton.setAttribute("aria-disabled", "false");
-            continueButton.style.pointerEvents = "auto"
-            continueButton.style.opacity = "1";
-          } else {
-            continueButton.setAttribute("aria-disabled", "true");
-            continueButton.style.pointerEvents = "none"
-            continueButton.style.opacity = "0.3"; 
-          }  
-        });
-    }
+    document
+      .getElementById('termsCheckbox')
+      .addEventListener('change', function () {
+        if (this.checked) {
+          continueButton.setAttribute('aria-disabled', 'false');
+          continueButton.style.pointerEvents = 'auto';
+          continueButton.style.opacity = '1';
+        } else {
+          continueButton.setAttribute('aria-disabled', 'true');
+          continueButton.style.pointerEvents = 'none';
+          continueButton.style.opacity = '0.3';
+        }
+      });
+  }
 }
 
 setupPwdTogglers();
